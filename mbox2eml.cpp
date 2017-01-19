fd937d3c1ec1c3558a67d4f4c8068988b0e96618
giomba <giomba@glgprograms.it>
2017-01-19 17:28:31 +0100

    Added basic usage in comments

--- mbox2eml.cpp
+++ mbox2eml.cpp
@@ -1,3 +1,14 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Basic usage:
+ *  ./this-bin < SomeMboxFile
+ * creates one eml file for every email contained in SomeMboxFile
+ */
+
 #include <iostream>
 #include <cstring>
 #include <fstream>
