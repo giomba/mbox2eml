4cc9fcbb63df3e718e402920a556ccb5c5b62b8f
giomba <giomba@glgprograms.it>
2018-06-02 14:58:20 +0200

    Core rewritten with better finite state machine

--- mbox2eml.cpp
+++ mbox2eml.cpp
@@ -9,43 +9,76 @@
  * creates one eml file for every email contained in SomeMboxFile
  */
 
+/* Include files for external symbols in STL */
+#include <algorithm>
 #include <iostream>
 #include <cstring>
+#include <cmath>
 #include <fstream>
 
-const std::string FILENAME_PREFIX = "mbox2eml-out-";
+/* Commonly used const strings */
 const std::string FILENAME_EXTENSION = ".eml";
+const std::string FILENAME_PREFIX = "mbox2eml-out-";
+
+const std::string FROM_STRING = "From ";
+const std::string SUBJECT_STRING = "Subject: ";
+
+/* FSM States */
+enum Status { ST_SEARCH, ST_SPONGE, ST_END };
+
+/* Functions declarations */
+void save(const std::string, const std::string&);
 
 int main(int argc, char** argv) {
+  Status status = ST_SEARCH;
   std::string line;
-  std::string from;
+  std::string sponge;
+  std::string subject;
 
-  std::fstream file;
   std::string filename;
-  int index = 0;
+  int index = 1;
 
-  filename = FILENAME_PREFIX + std::to_string(index) + FILENAME_EXTENSION;
-  file.open(filename, std::ios::out);
-
-  if (! file) { std::cerr << "[E] Can not open file" << std::endl; return 1; }
-
-  while (std::cin.good()) {
-    std::getline(std::cin, line);
-    from = line.substr(0, 5);
-    if (from.compare("From ") == 0) {
-      file.close();
-      ++index;
-      filename = FILENAME_PREFIX + std::to_string(index) + FILENAME_EXTENSION;
-      file.open(filename, std::ios::out);
-      if (! file) { std::cerr << "[E] Can not open file" << std::endl; return 1; }
-      file << line << std::endl;
-    }
-    else {
-      file << line << std::endl;
+  while (status != ST_END) {
+    switch (status) {
+      case ST_SEARCH:
+        std::getline(std::cin, line);
+        if (! std::cin.good()) {
+          status = ST_END;
+          break;
+        }
+        if (line.substr(0, FROM_STRING.length()).compare(FROM_STRING) == 0) {
+          status = ST_SPONGE;
+        }
+        break;
+      case ST_SPONGE:
+        sponge = sponge + line + '\n';
+        std::getline(std::cin, line);
+        if (line.substr(0, SUBJECT_STRING.length()).compare(SUBJECT_STRING) == 0) subject = line.substr(SUBJECT_STRING.length());
+        if (line.substr(0, FROM_STRING.length()).compare(FROM_STRING) == 0 || ! std::cin.good()) {
+          filename = FILENAME_PREFIX + std::string(4 - (int)log10(index), '0') + std::to_string(index) + '-' + subject + FILENAME_EXTENSION;
+          save(filename, sponge);
+          ++index;
+          sponge.clear();
+          if (! std::cin.good()) status = ST_END;
+        }
+        break;
+      case ST_END:
+        break;
     }
   }
 
-  file.close();
-
   return 0;
+}
+
+void save(const std::string filename, const std::string& content) {
+  std::fstream file;
+  std::string name = filename;
+
+  std::replace(name.begin(), name.end(), '/', '-');
+
+  // std::clog << "Writing " << name << std::endl;
+
+  file.open(name, std::ios::out);
+  file << content << std::endl;
+  file.close();
 }
