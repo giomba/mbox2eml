73e01aa3192493b2ec89c7ed713914c0408b351a
giomba <giomba@glgprograms.it>
2020-01-31 16:42:48 +0100

    Fixed performance issue with string allocation
    plus added some nice output on Linux

--- mbox2eml.cpp
+++ mbox2eml.cpp
@@ -17,47 +17,69 @@
 #include <fstream>
 #include <regex>
 
-/* Commonly used const strings */
+#include <csignal>
+#include <unistd.h>
+
+/* Commonly used const */
 const std::string FILENAME_EXTENSION = ".eml";
 const std::string FILENAME_PREFIX = "mbox2eml-out-";
 
 const std::regex SEPARATOR_R = std::regex("^From .*@.*\\..*( ){1,}[A-Z][a-z]{2} [A-Z][a-z]{2} \\d{1,2} \\d{1,2}:\\d{1,2}:\\d{1,2} \\d{4}$");
 const std::string SUBJECT_STRING = "Subject: ";
 
+const int RESERVED_MAIL_SIZE = 32 * (1 << 20);  // 32 MiB
+
 /* FSM States */
 enum Status { ST_SEARCH, ST_SPONGE, ST_END };
 
+/* Global variables */
+static unsigned long readbytes = 0;
+
 /* Functions declarations */
 void save(const std::string, const std::string&);
+void alarm_handler(int);
 
 int main(int argc, char** argv) {
+  /* variables declaration */
   Status status = ST_SEARCH;
   std::string line;
   std::string sponge;
+    sponge.reserve(RESERVED_MAIL_SIZE);
   std::string subject;
 
   std::string filename;
   int index = 1;
 
+  /**/
+  signal(SIGALRM, alarm_handler);
+  alarm(1);
+
   while (status != ST_END) {
     switch (status) {
+      /* Status Search: search for starting point of next email... */
       case ST_SEARCH:
         std::getline(std::cin, line);
         if (! std::cin.good()) {
           status = ST_END;
           break;
         }
-        // if (line.substr(0, FROM_STRING.length()).compare(FROM_STRING) == 0) {
+
+        /* if starting point is found, ... */
         if (std::regex_match(line, SEPARATOR_R)) {
           status = ST_SPONGE;
         }
         break;
+      /* Status Sponge: suck the next email like a sponge, up to the next separator */
       case ST_SPONGE:
-        sponge = sponge + line + '\n';
+        readbytes += line.length();
+        sponge.append(line);
+        sponge.append("\n");
         std::getline(std::cin, line);
+        /* detect mail subject, in order to name the .eml file after it */
         if (line.substr(0, SUBJECT_STRING.length()).compare(SUBJECT_STRING) == 0) {
             subject = line.substr(SUBJECT_STRING.length());
         }
+        /* detect new separator, save, then pass to next email */
         if (std::regex_match(line, SEPARATOR_R) || ! std::cin.good()) {
           filename = FILENAME_PREFIX + std::string(4 - (int)log10(index), '0') + std::to_string(index) + '-' + subject + FILENAME_EXTENSION;
           save(filename, sponge);
@@ -81,9 +103,17 @@ void save(const std::string filename, const std::string& content) {
 
   std::replace(name.begin(), name.end(), '/', '-');
 
-  // std::clog << "Writing " << name << std::endl;
-
   file.open(name, std::ios::out);
   file << content << std::endl;
   file.close();
+}
+
+/* print current number of bytes parsed */
+void alarm_handler(int signum) {
+  unsigned long size = readbytes / (1 << 10);
+  char unit = 'K';
+
+  std::clog << "read " << size << unit << std::endl;
+
+  alarm(2);
 }
